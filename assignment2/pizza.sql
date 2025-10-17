d152986571fbdcad3a9011fb40188a60c110f51b
markaxelus <mrkaxelus@gmail.com>
2025-10-17 00:32:01 -0700

    a2

---
+++ assignment2/pizza.sql
@@ -0,0 +1,154 @@
+/*
+Pizza Database
+
+Instructions:
+- This part of the assignment uses a small relational schema with data 
+  about people, pizzerias, pizzas, and prices.
+
+- The script below:
+    * Creates the tables (Person, Frequents, Eats, Serves).
+    * Populates them with sample data.
+
+- Your task:
+    * Write SQL queries to answer each question (Q1–Q7).
+    * For each question, replace the "Your query here" placeholder 
+      with your solution.
+    * Write the corresponding Relational Algebra (RA) expressions 
+      for these questions in a separate PDF file named pizza_ra.pdf.
+      - Handwritten RA is fine, as long as it is clear and legible.
+
+Submission:
+- Submit this completed SQL file with all your queries filled in.
+- Submit the pizza_ra.pdf file with your RA solutions.
+- Ensure your SQL queries run correctly against the provided schema.
+*/
+
+
+/* Create the schema for our tables */
+create table Person(name VARCHAR(30), age int, gender VARCHAR(10));
+create table Frequents(name VARCHAR(30), pizzeria VARCHAR(30));
+create table Eats(name VARCHAR(30), pizza VARCHAR(30));
+create table Serves(pizzeria VARCHAR(30), pizza VARCHAR(30), price float);
+
+/* Populate the tables with our data */
+insert into Person values('Amy', 16, 'female');
+insert into Person values('Ben', 21, 'male');
+insert into Person values('Cal', 33, 'male');
+insert into Person values('Dan', 13, 'male');
+insert into Person values('Eli', 45, 'male');
+insert into Person values('Fay', 21, 'female');
+insert into Person values('Gus', 24, 'male');
+insert into Person values('Hil', 30, 'female');
+insert into Person values('Ian', 18, 'male');
+
+insert into Frequents values('Amy', 'Pizza Hut');
+insert into Frequents values('Ben', 'Pizza Hut');
+insert into Frequents values('Ben', 'Chicago Pizza');
+insert into Frequents values('Cal', 'Straw Hat');
+insert into Frequents values('Cal', 'New York Pizza');
+insert into Frequents values('Dan', 'Straw Hat');
+insert into Frequents values('Dan', 'New York Pizza');
+insert into Frequents values('Eli', 'Straw Hat');
+insert into Frequents values('Eli', 'Chicago Pizza');
+insert into Frequents values('Fay', 'Dominos');
+insert into Frequents values('Fay', 'Little Caesars');
+insert into Frequents values('Gus', 'Chicago Pizza');
+insert into Frequents values('Gus', 'Pizza Hut');
+insert into Frequents values('Hil', 'Dominos');
+insert into Frequents values('Hil', 'Straw Hat');
+insert into Frequents values('Hil', 'Pizza Hut');
+insert into Frequents values('Ian', 'New York Pizza');
+insert into Frequents values('Ian', 'Straw Hat');
+insert into Frequents values('Ian', 'Dominos');
+
+insert into Eats values('Amy', 'pepperoni');
+insert into Eats values('Amy', 'mushroom');
+insert into Eats values('Ben', 'pepperoni');
+insert into Eats values('Ben', 'cheese');
+insert into Eats values('Cal', 'supreme');
+insert into Eats values('Dan', 'pepperoni');
+insert into Eats values('Dan', 'cheese');
+insert into Eats values('Dan', 'sausage');
+insert into Eats values('Dan', 'supreme');
+insert into Eats values('Dan', 'mushroom');
+insert into Eats values('Eli', 'supreme');
+insert into Eats values('Eli', 'cheese');
+insert into Eats values('Fay', 'mushroom');
+insert into Eats values('Gus', 'mushroom');
+insert into Eats values('Gus', 'supreme');
+insert into Eats values('Gus', 'cheese');
+insert into Eats values('Hil', 'supreme');
+insert into Eats values('Hil', 'cheese');
+insert into Eats values('Ian', 'supreme');
+insert into Eats values('Ian', 'pepperoni');
+
+insert into Serves values('Pizza Hut', 'pepperoni', 12);
+insert into Serves values('Pizza Hut', 'sausage', 12);
+insert into Serves values('Pizza Hut', 'cheese', 9);
+insert into Serves values('Pizza Hut', 'supreme', 12);
+insert into Serves values('Little Caesars', 'pepperoni', 9.75);
+insert into Serves values('Little Caesars', 'sausage', 9.5);
+insert into Serves values('Little Caesars', 'cheese', 7);
+insert into Serves values('Little Caesars', 'mushroom', 9.25);
+insert into Serves values('Dominos', 'cheese', 9.75);
+insert into Serves values('Dominos', 'mushroom', 11);
+insert into Serves values('Straw Hat', 'pepperoni', 8);
+insert into Serves values('Straw Hat', 'cheese', 9.25);
+insert into Serves values('Straw Hat', 'sausage', 9.75);
+insert into Serves values('New York Pizza', 'pepperoni', 8);
+insert into Serves values('New York Pizza', 'cheese', 7);
+insert into Serves values('New York Pizza', 'supreme', 8.5);
+insert into Serves values('Chicago Pizza', 'cheese', 7.75);
+insert into Serves values('Chicago Pizza', 'supreme', 8.5);
+
+
+
+
+/*Q1 (1 pt)
+Find all persons under the age of 18.*/
+
+/*Your query here*/
+
+
+/*Q2 (2 pts)
+Find all pizzerias that serve at least one pizza Amy eats for less than $10. 
+Output: pizzeria name, pizza name, price.*/
+
+/*Your query here*/
+
+
+/*Q3 (2 pts)
+Find all pizzerias frequented by at least one person under the age of 18. 
+Output: pizzeria name, person's name, person's age.*/
+
+/*Your query here*/
+
+
+/*Q4 (2 pts)
+Find all pizzerias frequented by at least one person under 18 and at least one person over 30. 
+Output: pizzeria name.*/
+
+/*Your query here*/
+
+
+/*Q5 (2 pts)
+Find all pizzerias frequented by at least one person under 18 and at least one person over 30. 
+Output: all quintuples (pizzeria, person1, age1, person2, age2), 
+where person1 is under 18 and person2 is over 30.*/
+
+/*Your query here*/
+
+
+/*Q6 (2 pts)
+For each person, find how many types of pizzas they eat. 
+Show only those who eat at least two types of pizza. 
+Sort results in descending order by number of pizza types.*/
+
+/*Your query here*/
+
+
+/*Q7 (2 pts)
+For each type of pizza, find its average price. 
+Sort results in descending order by average price.*/
+
+/*Your query here*/
---
+++ assignment2/soccer.sql
@@ -0,0 +1,204 @@
+/*
+This part focuses on soccer analytics using SQL.
+
+1. Data Setup
+   - Import the provided CSV files:
+       england.csv, france.csv, germany.csv, italy.csv
+   - Use DataGrip to import the files and automatically create the tables.
+   - The resulting tables will be named: England, France, Germany, and Italy.
+   - Each table contains over 100 years of soccer game statistics.
+   - When importing, carefully assign the correct datatypes for each column.
+
+2. Tasks
+   - Write SQL statements to answer the questions provided.
+   - For each question, replace the placeholder text 
+     ("Your query here") with your SQL query.
+
+3. Submission
+   - Submit this SQL file with your completed queries.
+   - Submit one PDF file (soccer_viz.pdf) containing your visualizations for Questions 2, 7, 9, and 10.
+     * Clearly label each visualization with the corresponding question number (e.g., Q2, Q7, Q9, Q10).
+*/
+
+
+
+/*Q1 (1 pt)
+Find all the games in England between seasons 1920 and 1999 such that the total goals are at least 13.
+Order by total goals descending.*/
+
+/*Your query here*/
+
+/*Sample result
+1935-12-26,1935,Tranmere Rovers,Oldham Athletic,13,4,3,17,9,H
+1958-10-11,1958,Tottenham Hotspur,Everton,10,4,1,14,6,H
+...*/
+
+
+/*Q2 (2 pt)
+For each total goal result, find how many games had that result.
+Use the england table and consider only the seasons since 1980.
+Order by total goal.*/
+
+/*Your query here*/
+
+/*Sample result
+0,6085
+1,14001
+...*/
+
+/*Visualize the results using a barchart.*/
+
+
+/*Q3 (2 pt)
+Find for each team in England in tier 1 the total number of games played since 1980.
+Report only teams with at least 300 games.
+
+Hint. Find the number of games each team has played as "home".
+Find the number of games each team has played as "visitor".
+Then union the two and take the sum of the number of games.
+*/
+
+/*Your query here*/
+
+/*Sample result
+Everton,1451
+Liverpool,1451
+...*/
+
+
+/*Q4 (1 pt)
+For each pair team1, team2 in England, in tier 1,
+find the number of home-wins since 1980 of team1 versus team2.
+Order the results by the number of home-wins in descending order.
+
+Hint. After selecting the tuples needed (... WHERE tier=1 AND ...) do a GROUP BY home, visitor.
+*/
+
+/*Your query here*/
+
+/*Sample result
+Manchester United,Tottenham Hotspur,27
+Arsenal,Everton,26
+...*/
+
+
+/*Q5 (1 pt)
+For each pair team1, team2 in England in tier 1
+find the number of away-wins since 1980 of team1 versus team2.
+Order the results by the number of away-wins in descending order.*/
+
+/*Your query here*/
+
+/*Sample result
+Manchester United,Aston Villa,18
+Manchester United,Everton,17
+...*/
+
+
+/*Q6 (2 pt)
+For each pair team1, team2 in England in tier 1 report the number of home-wins and away-wins
+since 1980 of team1 versus team2.
+Order the results by the number of away-wins in descending order.
+
+Hint. Join the results of the two previous queries. To do that you can use those
+queries as subqueries. Remove their ORDER BY clause when making them subqueries.
+Be careful on the join conditions.
+*/
+
+/*Your query here*/
+
+/*Sample result
+Manchester United,Aston Villa,26,18
+Arsenal,Aston Villa,20,17
+...*/
+
+--Create a view, called Wins, with the query for the previous question.
+
+
+/*Q7 (2 pt)
+For each pair ('Arsenal', team2), report the number of home-wins and away-wins
+of Arsenal versus team2 and the number of home-wins and away-wins of team2 versus Arsenal
+(all since 1980).
+Order the results by the second number of away-wins in descending order.
+Use view W1.*/
+
+/*Your query here*/
+
+/*Sample result
+Arsenal,Liverpool,14,8,20,11
+Arsenal,Manchester United,16,5,19,11
+...*/
+
+/*Drop view Wins.*/
+DROP VIEW Wins;
+
+/*Build two bar-charts, one visualizing the two home-wins columns, and the other visualizing the two away-wins columns.*/
+
+
+/*Q8 (2 pt)
+Winning at home is easier than winning as visitor.
+Nevertheless, some teams have won more games as a visitor than when at home.
+Find the team in Germany that has more away-wins than home-wins in total.
+Print the team name, number of home-wins, and number of away-wins.*/
+
+/*Your query here*/
+
+/*Sample result
+Wacker Burghausen	...	...*/
+
+
+/*Q9 (3 pt)
+One of the beliefs many people have about Italian soccer teams is that they play much more defense than offense.
+Catenaccio or The Chain is a tactical system in football with a strong emphasis on defence.
+In Italian, catenaccio means "door-bolt", which implies a highly organised and effective backline defence
+focused on nullifying opponents' attacks and preventing goal-scoring opportunities.
+In this question we would like to see whether the number of goals in Italy is on average smaller than in England.
+
+Find the average total goals per season in England and Italy since the 1970 season.
+The results should be (season, england_avg, italy_avg) triples, ordered by season.
+
+Hint.
+Subquery 1: Find the average total goals per season in England.
+Subquery 2: Find the average total goals per season in Italy
+   (there is no totgoal in table Italy. Take hgoal+vgoal).
+Join the two subqueries on season.
+*/
+
+/*Your query here*/
+
+--Build a line chart visualizing the results. What do you observe?
+
+/*Sample result
+1970,2.5290927021696252,2.1041666666666667
+1971,2.5922090729783037,2.0125
+...*/
+
+
+/*Q10 (3 pt)
+Find the number of games in France and England in tier 1 for each goal difference.
+Return (goaldif, france_games, eng_games) triples, ordered by the goal difference.
+Normalize the number of games returned dividing by the total number of games for the country in tier 1,
+e.g. 1.0*COUNT(*)/(select count(*) from france where tier=1)  */
+
+/*Your query here*/
+
+/*Sample result
+-8,0.00011369234850494562,0.000062637018477920450987
+-7,0.00011369234850494562,0.00010439503079653408
+...*/
+
+/*Visualize the results using a barchart.*/
+
+
+/*Q11 (2 pt)
+Find all the seasons when England had higher average total goals than France.
+Consider only tier 1 for both countries.
+Return (season,england_avg,france_avg) triples.
+Order by season.*/
+
+/*Your query here*/
+
+/*Sample result
+1936,3.3658008658008658,3.3041666666666667
+1952,3.2640692640692641,3.1437908496732026
+...*/
